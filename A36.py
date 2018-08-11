69acc0ef23691190b9973adc357a3f56bb6adc62
Yuji Nishida <yuji.nishida@example.com>
2018-08-12 08:45:17 +0900

    Added A36.py without regular expression.

---
+++ A36.py
@@ -0,0 +1,36 @@
+def is_ip_number(ips):
+    if len(ips) == 4:
+        for ip in ips:
+            try:
+                int(ip) 
+            except e:
+                return False
+    else:
+        return False
+
+    return True
+
+def is_ip_range(ips):
+    for ip in ips:
+        if 0 <= int(ip) and int(ip) <= 255:
+            pass
+        else:
+            return False
+    return True
+
+def is_correct_ip_format(ip):
+
+    ips = ip.split(".")
+
+    if not is_ip_number(ips):
+        return False
+
+    if not is_ip_range(ips):
+        return False
+
+    return True
+
+print(is_correct_ip_format("192.168.0.1"))
+print(is_correct_ip_format("192.168.0.1000"))
+print(is_correct_ip_format("192.168.1"))
+print(is_correct_ip_format("192.168.0.0.1"))
