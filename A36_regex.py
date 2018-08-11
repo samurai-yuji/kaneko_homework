cc0b7bdb8f73d90b069983850e54e7d63218d22b
Yuji Nishida <yuji.nishida@example.com>
2018-08-12 08:49:39 +0900

    Added A36.py with regular expression.

---
+++ A36_regex.py
@@ -0,0 +1,12 @@
+import re
+
+def is_correct_ip_format(ip):
+    if re.match("^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$",ip):
+        return True
+    else:
+        return False
+
+print(is_correct_ip_format("192.168.0.1"))
+print(is_correct_ip_format("192.168.0.1000"))
+print(is_correct_ip_format("192.168.1"))
+print(is_correct_ip_format("192.168.0.0.1"))
